27789f8659def374ababb89d8c9749a0b29eef40
Boussant-roux Luc <luc.boussantr@gmail.com>
2018-05-10 17:23:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function testRetard() {
 
 var http = require("http");
 setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
+    http.get("http://eazyboss.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
 
 testRetard()
